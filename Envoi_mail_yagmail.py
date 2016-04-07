0bbf95f033fd1c7049077aefb4d6723af2fa6294
joris-s <joris.serrand@rascol.net>
2016-04-07 17:14:23 +0200

    modif mail

--- Envoi_mail_yagmail.py
+++ Envoi_mail_yagmail.py
@@ -1,9 +1,7 @@
-
-
 import yagmail #Import de la librairie yagmail
 
 yag = yagmail.SMTP('emetteur@gmail.com', 'password') #Paramétrage du mail GMAIL emetteur et du mot de passe
 
 contents = ['This is the body, and here is just text http://somedomain/image.png',
             'You can find an audio file attached.']
-yag.send('j.serrand@icloud.com', 'Test Yagmail', contents)
+yag.send('test@test.com', 'Test Yagmail', contents)
